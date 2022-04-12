b7b039e76762300a9fb41ed3856f6c960e3d7197
Ibrahim Chand <31962750+ibchand@users.noreply.github.com>
2022-04-12 17:07:05 -0400

    Implemented P2P System

---
+++ client_2.py
@@ -0,0 +1,51 @@
+# Python program to implement client side of chat room
+import socket 
+import select 
+import sys
+from _thread import *
+import time
+
+if len(sys.argv) != 2:
+  print ("Correct usage: script, IP address")
+  exit()
+IP_address = str(sys.argv[1])
+
+# Server
+server_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_s.bind((IP_address, 8082))
+server_s.listen(100)
+time.sleep(3)
+
+# Client
+server_c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_c.connect((IP_address, 8081))
+
+def clientthread(conn_s, addr):
+  conn_s.send("Welcome to the chatroom - Client 1".encode('utf-8'))
+
+  while True:
+    # CLIENT SIDE
+    sockets_list = [sys.stdin, server_c]
+
+    read_sockets,write_socket,error_socket = select.select(sockets_list,[],[])
+
+    for socks in read_sockets:
+      if socks == server_c:
+        message = socks.recv(2048)
+        print("<" + addr[0] + "> " + message.decode('utf-8'))
+      else:
+        message = sys.stdin.readline()
+        conn_s.send(message.encode('utf-8'))
+        sys.stdout.write("<Client2>")
+        sys.stdout.write(message)
+        sys.stdout.flush()
+
+while True:
+  conn, addr = server_s.accept()
+
+  print (addr[0] + " connected")
+
+  start_new_thread(clientthread,(conn,addr))
+
+server_s.close()
--- server.py
+++ server.py
@@ -3,9 +3,6 @@ import select
 import sys
 from _thread import *
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
 if len(sys.argv) != 3:
   print ("Correct usage: script, IP address, port number")
   exit()
@@ -13,51 +10,71 @@ if len(sys.argv) != 3:
 IP_address = str(sys.argv[1])
 PORT = int(sys.argv[2])
 
-server.bind((IP_address, PORT))
+server_1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-server.listen(100)
+server_2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-list_of_clients = []
+server_1.bind((IP_address, 8081))
 
-def clientthread(conn, addr):
-  conn.send("Welcome to the chatroom".encode('utf-8'))
+server_2.bind((IP_address, 8082))
+
+server_1.listen(100)
+
+server_2.listen(100)
+
+list_of_clients_1 = []
+
+list_of_clients_2 = []
+
+# conn_r = Receiving From
+# conn_s = Sending To
+def clientthread(conn_r, conn_s, addr, list_of_clients):
+  conn_s.send("Welcome to the chatroom".encode('utf-8'))
 
   while True:
     try:
-      message = conn.recv(2048)
+      message = conn_r.recv(2048)
       message = message.decode('utf-8')
       if message:
         message_2 = "<" + addr[0] + "> " + message
 
         print(message_2)
 
-        broadcast(message_2, conn)
+        broadcast(message_2, conn_r)
       else:
-        remove(conn)
+        remove(conn_s)
 
     except:
       continue
 
-def broadcast(message, connection):
+def broadcast(message, connection, list_of_clients):
   for clients in list_of_clients:
     if clients!=connection:
       try:
         clients.send(message.encode('utf-8'))
       except:
         clients.close()
-        remove(clients)
+        remove(clients, list_of_clients)
 
-def remove(connection):
+def remove(connection, list_of_clients):
   if connection in list_of_clients:
     list_of_clients.remove(connection)
 
 while True:
-  conn, addr = server.accept()
-  list_of_clients.append(conn)
+  conn_1, addr_1 = server_1.accept()
+  conn_2, addr_2 = server_2.accept()
+  list_of_clients_1.append(conn_1)
+  list_of_clients_2.append(conn_2)
 
-  print (addr[0] + " connected")
+  print (addr_1[0] + " connected")
+  print (addr_2[0] + " connected")
 
-  start_new_thread(clientthread,(conn,addr))	
+  start_new_thread(clientthread,(conn_1, conn_2, addr_1,list_of_clients_2))	
+  start_new_thread(clientthread,(conn_2, conn_1, addr_2,list_of_clients_1))
 
-conn.close()
-server.close()
+conn_1.close()
+conn_2.close()
+server_1.close()
+server_2.close()
